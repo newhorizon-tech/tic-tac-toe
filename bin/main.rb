211f9007f8cfca6812ab15695fcd933a711ba6d6
Nkuria <nkuria01@gmail.com>
2020-10-27 11:33:39 +0300

    Added the board print method

--- bin/main.rb
+++ bin/main.rb
@@ -7,6 +7,17 @@ puts "Enter your name player 2"
 p2 = gets.chomp
 return [p1, p2]
 end
+
+def print_board(input)
+        puts "|1|2|3|"
+        puts "_ _ _"
+        puts "|4|5|6|"
+        puts "_ _ _"
+        puts "|7|8|9|"
+        puts "_ _ _ "
+end
+
+
 player_list = player_name
 p player_list
 
@@ -18,12 +29,23 @@ while !win do
     puts "Enter your choice (player 1) "
     turn_p1.push(gets.chomp.to_i)
     #check if move is valid
-    p board
+    print_board(board)
+    if check_win == true
+         win = true 
+        puts "Player 1 wins"
+        break
+    end
     puts "Enter your choice (player 2)"
     turn_p2.push(gets.chomp.to_i)
     #check if move is valid
-    p board
+    print_board(board)
+    if check_win == true
+        win = true 
+       puts "Player 2 wins"
+       break
+   end
 end
+
 
     
 
