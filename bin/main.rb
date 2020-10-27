5319c0e508b76002b92e3c7775cc5c29fbb7466f
Nkuria <nkuria01@gmail.com>
2020-10-27 11:19:07 +0300

    Add basic user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,29 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World!'
+def player_name
+puts "Enter your name player 1"
+p1 = gets.chomp
+puts "Enter your name player 2"
+p2 = gets.chomp
+return [p1, p2]
+end
+player_list = player_name
+p player_list
+
+board = [0,0,0,0,0,0,0,0,0]
+turn_p1 = []
+turn_p2 = []
+win = false
+while !win do
+    puts "Enter your choice (player 1) "
+    turn_p1.push(gets.chomp.to_i)
+    #check if move is valid
+    p board
+    puts "Enter your choice (player 2)"
+    turn_p2.push(gets.chomp.to_i)
+    #check if move is valid
+    p board
+end
+
+    
+
