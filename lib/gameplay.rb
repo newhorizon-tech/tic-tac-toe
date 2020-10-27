28d2a6c927e6135d246790c1dc61308843dc1dba
newhorizon-tech <alaukikinbox@gmail.com>
2020-10-27 16:25:31 +0530

    Add update_board method

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -2,18 +2,11 @@ class Gameplay
   def initialize
     @board = []
     9.times { @board.push(0) }
-    @board[2] = 1
-    @board[5] = 1
-    @board[7] = 1
   end
 
   def valid_move(move)
     valid = true
     error = ''
-    puts "Move is #{move}"
-    puts "Move<0: #{move < 0}"
-    puts "Move>9: #{move > 9}"
-    puts "Move < 1 and Move > 9: #{move < 1 or move > 9}"
     if move < 1 or move > 9
       valid = false
       error = 'Number is not between 0 and 9'
@@ -23,12 +16,32 @@ class Gameplay
     end
     [valid, error]
   end
+
+  def update_board(move, player)
+    @board[move - 1] = player
+  end
+
+  def print_board
+    puts '-' * 40
+    puts "|#{@board[0]}|#{@board[1]}|#{@board[2]}|"
+    puts ' - - -'
+    puts "|#{@board[3]}|#{@board[4]}|#{@board[5]}|"
+    puts ' - - -'
+    puts "|#{@board[6]}|#{@board[7]}|#{@board[8]}|"
+    puts ' - - -'
+    puts '-' * 40
+  end
 end
 
 test = Gameplay.new
-
-(-3..12).to_a.each do |i|
-  test_move = test.valid_move(i)
-  p test_move
-  puts '-' * 40
-end
+puts test.valid_move(3)
+test.update_board(3, 'x')
+test.update_board(1, 'o')
+test.update_board(2, 'x')
+test.update_board(4, 'o')
+test.update_board(5, 'x')
+test.update_board(9, 'o')
+test.update_board(8, 'x')
+test.update_board(7, 'o')
+test.update_board(6, 'x')
+test.print_board
