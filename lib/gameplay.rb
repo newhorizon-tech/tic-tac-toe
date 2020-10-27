04f4e3bd0249cb543edaef9597f4222ef519c08f
newhorizon-tech <alaukikinbox@gmail.com>
2020-10-27 18:58:10 +0530

    Implement who_won method in gameplay.rb

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -21,6 +21,25 @@ class Gameplay
     @board[move - 1] = player
   end
 
+  def who_won
+    horizontal = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    vertical = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
+    diagonal = [[0, 4, 8], [2, 4, 6]]
+    winning_combo = [horizontal, vertical, diagonal].flatten(1)
+    winning_combo.each do |combo|
+      puts "Combo is #{combo}"
+      if combo.all? { |position| @board[position].eql? 'x' } # Checking if 'x' has won
+        return 1
+      elsif combo.all? { |position| @board[position].eql? 'o' } # Checking if 'o' has won
+        return 2
+      elsif combo.all? { |position| !@board[position].zero? } # Checking for a draw
+        return 3
+      else
+        return 0
+      end
+    end
+  end
+
   def print_board
     puts '-' * 40
     puts "|#{@board[0]}|#{@board[1]}|#{@board[2]}|"
@@ -41,7 +60,7 @@ test.update_board(2, 'x')
 test.update_board(4, 'o')
 test.update_board(5, 'x')
 test.update_board(9, 'o')
-test.update_board(8, 'x')
 test.update_board(7, 'o')
-test.update_board(6, 'x')
 test.print_board
+
+who_won
